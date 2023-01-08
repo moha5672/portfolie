4bfb4700eba81a385fa9ba97baef90df47e11773
Mohamad Hussein <hammoudi5672@gmail.com>
2023-01-08 20:01:10 +0100

    java

---
+++ hamburger.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".navTest");
+const webMenu = document.querySelector(".menu-website");
+
+function menuDisplay() {
+  console.log("xxxxxx");
+  menu.classList.toggle("appear");
+  menu.classList.contains("appear");
+  webMenu.classList.toggle("appear");
+  webMenu.classList.contains("appear");
+}
+
+menu.addEventListener("click", menuDisplay);
